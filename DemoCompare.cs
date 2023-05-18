760176683ae8b91e0cc24fa644e94507d4ed66f2
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-05-17 18:23:01 -0700

    Updated Notes

---
+++ DemoCompare.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_124_S23_L11_ICompareable
+{
+    internal class DemoCompare : IComparable<DemoCompare>
+    {
+        string _name;
+        int _number;
+        DateTime _date;
+
+        public DemoCompare(string name, int number, DateTime date)
+        {
+            _name = name;
+            _number = number;
+            _date = date;
+        }
+
+        public int CompareTo(DemoCompare? other)
+        {
+            if(_number > other._number)
+            {
+                return 1;
+            }
+            else if (_number < other._number)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} - Number: {_number} - DateTime: {_date.ToShortDateString()}";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Prog_124_S23_L11_ICompareable
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<DemoCompare> _demoList = new List<DemoCompare>();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            _demoList.Add(new DemoCompare("Will", 10, new DateTime(1974, 8, 4)));
+            _demoList.Add(new DemoCompare("Ronda", 15, new DateTime(1945, 8, 4)));
+            _demoList.Add(new DemoCompare("Meili", 1, new DateTime(1999, 8, 4)));
+            _demoList.Add(new DemoCompare("John", 24, new DateTime(2021, 8, 4)));
+            _demoList.Add(new DemoCompare("Kai", 55, new DateTime(1933, 8, 4)));
+            _demoList.Add(new DemoCompare("Henry", 111, new DateTime(1374, 8, 4)));
+            _demoList.Add(new DemoCompare("Arsh", 65, new DateTime(1174, 8, 4)));
+
+            _demoList.Sort();
+
+            lbList.ItemsSource = _demoList;
+
+            _demoList.Add(new DemoCompare("Sarah", 65, new DateTime(1174, 8, 4)));
+            _demoList.Add(new DemoCompare("Ednalynn", 65, new DateTime(1174, 8, 4)));
+            _demoList.Add(new DemoCompare("Cirella", 65, new DateTime(1174, 8, 4)));
+
+
         }
+
+
     }
 }
